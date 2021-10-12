ee6d4bb62a817e5e13111b9a3999fb547077e062
Vishal921120 <vishaldabas04@gmail.com>
2021-10-12 17:47:27 +0530

    add nginx

---
+++ client/src/index.js
@@ -0,0 +1,44 @@
+// Importing Env variables
+require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+
+// configs
+import googleAuthConfig from "./config/google.config";
+
+// microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+
+const zomato = express();
+
+// Application middle wares
+zomato.use(express.json());
+zomato.use(express.urlencoded({extended: false}));
+zomato.use(helmet());
+zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport cofiguration
+googleAuthConfig(passport);
+
+
+// Application Routes
+zomato.use("/auth", Auth);
+
+zomato.get("/", (req, res) => res.json({message : "setup success"}));
+zomato.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running "))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
